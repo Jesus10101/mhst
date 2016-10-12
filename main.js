4f75091e2b30afb8d59bd20346b05a7d4a769b74
KIRAcc <ppppppoa@gmail.com>
2016-10-12 17:05:33 +0900

    ちょっと微妙な機能追加
    ・アイテム個数をクリックして編集できるようになりました
    ・アイテム位置について、表示が「0番目」から開始となっていたため「」から開始するよう表示のみ変更しました（内部処理は0番目からで問題ありません）

--- main.js
+++ main.js
@@ -32,23 +32,36 @@ reader.onload = function(evt){
   $('#value_name').val(data.name);*/
 
   var count = 1;
-  data.item = [];
-  for(var i=0; i<=1496; i++) {
-    var n = i+1;
-    var addr = 0x10 + 8*(n-1);
+  data.item = {};
+  data.item_changed = {};
+  data.item_pos = {};
+  for(var pos=0; pos<=1496; pos++) {
+    var addr = 0x10 + 8*pos;
     var id = dataView.getUint16( addr , isLE );
     var idStr = ( '000' + id.toString(16).toUpperCase() ).slice(-4);
     var name = itemList[id];
     data.item[id] = dataView.getUint16( addr + 2 , isLE );
     if(data.item[id] === 0) continue;
+    data.item_pos[id] = pos;
     if(name === void(0)) name = '？？？？？？';
     var tr = $('<tr></tr>').appendTo('table#list_item > tbody');
     $('<th scope="row"></th>').html( count ).appendTo(tr);
     var td_addr = $('<td></td>').html( '0x' + ( '000' + addr.toString(16).toUpperCase() ).slice(-4) ).appendTo(tr);
-    var td_pos = $('<td></td>').html( (n-1) + '番目' ).appendTo(tr);
+    var td_pos = $('<td></td>').html( (pos+1) + '番目' ).appendTo(tr);
     var td_id = $('<td></td>').html( idStr ).appendTo(tr);
     var td_name = $('<td></td>').html( name ).appendTo(tr);
-    var td_num = $('<td></td>').html( data.item[id] ).appendTo(tr);
+    var td_num = $('<td></td>').html( data.item[id] ).data('item_id', id).appendTo(tr);
+
+    td_num.click(function(){
+      var id = $(this).data('item_id');
+      var value = prompt('新しい値を入力してください', data.item[id]);
+      if(value === null) return;
+      value = parseInt(value);
+      if( !(0 < value && value < 1000) ) {alert('正常な値（1-999個）を入力してください'); return}
+
+      data.item_changed[id] = value;
+      $(this).html(value);
+    });
     count++;
   }
 }
@@ -62,7 +75,15 @@ function saveFile() {
     buffer.merge(newNameBuf, 0xC7A1C);
   }*/
   var dataView_new = new DataView(fileBuf_new);
+
   if(data.zenny != $('#value_zenny').val()) dataView_new.setUint32(0x5B404, parseInt($('#value_zenny').val(), 10), isLE); // 所持金の場合
+
+  $.each(data.item_changed, function(id, val){ // アイテムの場合
+    var pos = data.item_pos[id];
+    var addr = 0x10 + 8*(pos) + 2;
+    alert(data.item[id] !== val);
+    if(data.item[id] !== val) dataView_new.setUint16(addr, val, isLE);
+  });
   makeBlob('mhr_game0.sav', fileBuf_new);
 }
 
@@ -90,6 +111,7 @@ function writeValue(buffer, name, value) {
     case 'name':
       break;
     case "zenny": // 所持金の場合
+      break;
     default:
 
   }
